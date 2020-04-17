cece49d2c8fabd53f28077d7b8796d1f21e1ec58
Ivaylo Markov <ivo@schupen.net>
2020-04-17 15:14:48 +0300

    Basic HTTP JSON export

--- main.go
+++ main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"regexp"
 	"strings"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/nxadm/tail"
 )
 
 var streamNameRegex = regexp.MustCompile(`\/hls\/(?P<streamName>.*)-\d+\.ts`)
 var streamViewers = map[string]map[string]struct{}{}
+var exportViewers = map[string]int{}
 
 var interval time.Duration
 var logFile string
+var httpBind string
 
 func init() {
 	flag.DurationVar(
@@ -26,6 +31,11 @@ func init() {
 	flag.StringVar(
 		&logFile, "logfile", "/var/log/nginx/access.log",
 		"Path to Nginx access log file",
+	)
+
+	flag.StringVar(
+		&httpBind, "http-bind", "",
+		"Address and port to bind HTTP JSON export to",
 	)
 
 	flag.Parse()
@@ -78,15 +88,37 @@ func countViews(logFile string) {
 		case line := <-lineChan:
 			processLine(line)
 		case <-ticker.C:
+			exportViewers = map[string]int{}
 			for streamName, viewers := range streamViewers {
 				fmt.Printf("stream=%s, viewers=%d\n", streamName, len(viewers))
+				exportViewers[streamName] = len(viewers)
 			}
 			streamViewers = map[string]map[string]struct{}{}
 		}
 	}
 }
 
+type httpViewsData struct {
+	Streams map[string]int `json:"streams"`
+}
+
+func runHTTPExport() {
+	fmt.Printf("Binding HTTP export on %s\n", httpBind)
+	r := mux.NewRouter()
+	r.HandleFunc(
+		"/views",
+		func(w http.ResponseWriter, r *http.Request) {
+			data := httpViewsData{exportViewers}
+			json.NewEncoder(w).Encode(data)
+		},
+	)
+	http.ListenAndServe(httpBind, r)
+}
+
 func main() {
 	fmt.Printf("Using interval=%s, logFile=%s\n", interval, logFile)
+	if httpBind != "" {
+		go runHTTPExport()
+	}
 	countViews(logFile)
 }
